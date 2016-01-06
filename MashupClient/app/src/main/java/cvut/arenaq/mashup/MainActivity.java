97b5f974ffb9cb4f436c32a36b1117de832f48a9
Petr Kuska <Petr.Kuska@e4t.cz>
2016-01-06 19:24:49 +0100

    Remove unused import

--- MashupClient/app/src/main/java/cvut/arenaq/mashup/MainActivity.java
+++ MashupClient/app/src/main/java/cvut/arenaq/mashup/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.io.IOException;
