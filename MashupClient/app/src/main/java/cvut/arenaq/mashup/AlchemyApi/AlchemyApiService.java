b1690c5384146534545a7c6b65e28e5b1e6c7ca6
Petr Kuska <Petr.Kuska@e4t.cz>
2016-01-06 20:55:09 +0100

    Fix inappropriate annotation usage in service interface

--- MashupClient/app/src/main/java/cvut/arenaq/mashup/AlchemyApi/AlchemyApiService.java
+++ MashupClient/app/src/main/java/cvut/arenaq/mashup/AlchemyApi/AlchemyApiService.java
@@ -3,11 +3,18 @@ package cvut.arenaq.mashup.AlchemyApi;
 import retrofit.Call;
 import retrofit.http.GET;
 import retrofit.http.Path;
+import retrofit.http.Query;
 
 public interface AlchemyApiService {
-    @GET("/calls/url/URLGetRankedKeywords?apikey=e9b2175fdaa36af4febe23ec32b3b9ad47154727&outputMode=json&url={url}")
-    Call<GetRankedKeywords> getRankedKeywords(@Path("url") String url);
+    @GET("/calls/url/URLGetRankedKeywords")
+    Call<GetRankedKeywords> getRankedKeywords(
+            @Query("apikey") String apikey,
+            @Query("outputMode") String outputMode,
+            @Query("url") String url);
 
-    @GET("/calls/url/URLGetRankedConcepts?apikey=e9b2175fdaa36af4febe23ec32b3b9ad47154727&outputMode=json&url={url}")
-    Call<GetRankedConcepts> getRankedConcepts(@Path("url") String url);
+    @GET("/calls/url/URLGetRankedConcepts")
+    Call<GetRankedConcepts> getRankedConcepts(
+            @Query("apikey") String apikey,
+            @Query("outputMode") String outputMode,
+            @Query("url") String url);
 }
