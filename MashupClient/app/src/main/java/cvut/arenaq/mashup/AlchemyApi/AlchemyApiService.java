ef48694993df913af973008521f2358cb693f993
Petr Kuška <kuska.petr@gmail.com>
2016-01-11 23:59:18 +0100

    Add new service to AlchemyApi

--- MashupClient/app/src/main/java/cvut/arenaq/mashup/AlchemyApi/AlchemyApiService.java
+++ MashupClient/app/src/main/java/cvut/arenaq/mashup/AlchemyApi/AlchemyApiService.java
@@ -2,7 +2,6 @@ package cvut.arenaq.mashup.AlchemyApi;
 
 import retrofit.Call;
 import retrofit.http.GET;
-import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface AlchemyApiService {
@@ -14,6 +13,12 @@ public interface AlchemyApiService {
 
     @GET("/calls/url/URLGetRankedConcepts")
     Call<GetRankedConcepts> getRankedConcepts(
+            @Query("apikey") String apikey,
+            @Query("outputMode") String outputMode,
+            @Query("url") String url);
+
+    @GET("/calls/url/URLGetRankedTaxonomy")
+    Call<GetRankedTaxonomy> getRankedTaxonomy(
             @Query("apikey") String apikey,
             @Query("outputMode") String outputMode,
             @Query("url") String url);
