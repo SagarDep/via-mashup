8dfa852bcf82537428bb67d42e22532409022ca3
Petr Kuska <Petr.Kuska@e4t.cz>
2016-01-12 19:48:59 +0100

    Switch to new layout

--- MashupClient/app/src/main/java/cvut/arenaq/mashup/MainActivity.java
+++ MashupClient/app/src/main/java/cvut/arenaq/mashup/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_table);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(IP_API_URL)
